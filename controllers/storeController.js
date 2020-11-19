8f4931b8063a5a5c0903ba1f3c4b139d2e0b2ad9
akash rattan <akashrattan43@Gmail.com>
2020-11-18 20:01:05 -0800

    form added also can edit

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,31 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
+  const store = await new Store(req.body).save();
   await store.save();
-  res.redirect('/')
+  req.flash(
+    "success",
+    `Sucessfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // query the database for a list of all stores
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render("editStore", { title: `Edit ${store.name}`, store: store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, 
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href='/store/${store.slug}'> View Store > <a </a>`)
+  res.redirect(`/stores/${store._id}/edit`)
 };
